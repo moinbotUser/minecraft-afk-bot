699c09c6086ffb1d6d8db2d07313c780d325e968
moinbotUser <zeeshans2139@outlook.com>
2026-03-15 18:42:02 +0100

    Update afk-bot.js

--- afk-bot.js
+++ afk-bot.js
@@ -1,1 +1,32 @@
+const bedrock = require("bedrock-protocol")
 
+function startBot() {
+
+const client = bedrock.createClient({
+  host: "BobuPower.aternos.me",
+  port: 27258,
+  username: "AFKBot"
+})
+
+client.on("join", () => {
+  console.log("Bot joined the server")
+})
+
+client.on("spawn", () => {
+  console.log("Bot spawned")
+
+  setInterval(() => {
+    console.log("Anti AFK running...")
+  }, 30000)
+})
+
+client.on("disconnect", () => {
+  console.log("Disconnected. Reconnecting...")
+  setTimeout(startBot, 5000)
+})
+
+client.on("error", err => console.log(err))
+
+}
+
+startBot()
